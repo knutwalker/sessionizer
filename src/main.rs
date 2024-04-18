2db1017dcb9e7b5aba047db7eda87b9bb62b6e3d
Paul Horn <knutwalker@users.noreply.github.com>
2024-04-18 14:06:01 +0200

    Update readme (#15)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-//! ## Select a new tmux session from a list of running sessions or a selection of projects.
-//!
-//! # Installation
-//!
-//! ```sh
-//! $ cargo install sessionizer
-//! ```
-//!
-//! # Usage
-//!
-//! ```sh
-//! $ sessionizer --help
-//! ```
-
 #![allow(unused_crate_dependencies)]
+#![doc = include_str!("./doc.md")]
 
 fn main() -> kommandozeile::Result<()> {
     let args = sessionizer::Args::init();
