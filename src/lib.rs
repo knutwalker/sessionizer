5034b3d47c65423e5647f623b4314d26d267f864
Paul Horn <dev@knutwalker.engineer>
2024-06-08 16:36:19 +0200

    Expand env vars

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@ fn apply_entry(entry: Entry, secure: bool) -> Result<Command> {
         Entry::Session(session) => Action::Attach { name: session.name },
     };
 
-    Ok(action::cmd(&action))
+    action::cmd(&action)
 }
 
 struct Thread<T> {
